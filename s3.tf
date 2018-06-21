060dc38c68db98cc1900c13c0ffac9d8d4284198
navgit <mail.navazish@gmail.com>
2018-06-21 11:26:11 +0000

    Pushing code

--- s3.tf
+++ s3.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "b" {
 		"Action": "s3:GetObject",
 		"Resource": "arn:aws:s3:::mybucket-462281/*"
 	}]
-}
+} 
 	website {
     index_document = "index.html"
     error_document = "error.html"
