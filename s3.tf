171b2890828fb204dd7d4b7eea4e0f2de79dea8c
navgit <mail.navazish@gmail.com>
2018-06-21 09:36:07 +0000

    Adding policy

--- s3.tf
+++ s3.tf
@@ -1,8 +1,27 @@
 resource "aws_s3_bucket" "b" {
     bucket = "mybucket-462281"
-    acl = "private"
+    acl = "public-read"
 
     tags {
         Name = "mybucket-462281"
     }
+	policy {
+	{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow", 
+            "Principal": "*", 
+            "Action": "s3:GetObject", 
+            "Resource": "arn:aws:s3:::[YOUR_BUCKET_NAME]/*" 
+        } 
+    ] 
+	}
+	
+	}
+	website {
+    index_document = "index.html"
+    error_document = "error.html"
+	EOF
+	}
 }
