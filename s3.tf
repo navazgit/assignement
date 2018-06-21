4b985e443326c8aceb4deb03bf131f60ed96c6d4
navgit <mail.navazish@gmail.com>
2018-06-21 10:29:30 +0000

    Pushing code

--- s3.tf
+++ s3.tf
@@ -5,10 +5,5 @@ resource "aws_s3_bucket" "b" {
     tags {
         Name = "mybucket-462281"
     }
-	role = "s3-mybucket-role-462281"
-	website {
-    index_document = "index.html"
-    error_document = "error.html"
 	
-	}
 }
