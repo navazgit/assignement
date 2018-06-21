247cdfaa8d11d30dd3887cb5381d69473a902099
navgit <mail.navazish@gmail.com>
2018-06-21 11:31:47 +0000

    Pushing code

--- s3.tf
+++ s3.tf
@@ -5,14 +5,15 @@ resource "aws_s3_bucket" "b" {
     tags {
         Name = "mybucket-462281"
     }
-	policy {
-			"Version": "2012-10-17",
-			"Statement": [{
-				"Effect": "Allow",
-				"Principal": "*",
-				"Action": "s3:GetObject",
-				"Resource": "arn:aws:s3:::mybucket-462281/*"
-			}]
+	policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [{
+		"Effect": "Allow",
+		"Principal": "*",
+		"Action": "s3:GetObject",
+		"Resource": "arn:aws:s3:::mybucket-462281/*"
+	}]
 } 
 	website {
     index_document = "index.html"
