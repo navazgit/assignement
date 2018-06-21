2a1339cb83573267b117edc316acc03159d3590a
navgit <mail.navazish@gmail.com>
2018-06-21 10:24:00 +0000

    Psuhing code

--- s3.tf
+++ s3.tf
@@ -6,23 +6,9 @@ resource "aws_s3_bucket" "b" {
         Name = "mybucket-462281"
     }
 	role = "s3-mybucket-role-462281"
-	/*policy {
-	{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow", 
-            "Principal": "*", 
-            "Action": "s3:GetObject", 
-            "Resource": "arn:aws:s3:::[YOUR_BUCKET_NAME]/*" 
-        } 
-    ] 
-	}
-	}
-	*/
 	website {
     index_document = "index.html"
     error_document = "error.html"
-	EOF
+	
 	}
 }
